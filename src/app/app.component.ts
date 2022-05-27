54960e6b1af173919f5dbb28d7698b94a22cef27
Matheus Moreira <matheus.c.g.moreira@gmail.com>
2022-05-26 21:46:37 -0300

    codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'news-app';
+  public title = 'news-app';
+  constructor() {}
+
+
 }
